6a1777da3f59979e42d12dca952bec446972b1ce
CyCJIuK47 <alexander.suslynets@gmail.com>
2024-01-20 22:33:59 +0200

    Add project skeleton

---
+++ src/app/containers/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Box>
+      <Typography>Hello, GraphAlchemy</Typography>
+    </Box>
+  );
+}
+
+export default App;
